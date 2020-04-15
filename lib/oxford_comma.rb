21f0f87083425ee972476366cc4a89c5989c8d07
Mark Cascardi <mcascardi@gmail.com>
2020-04-15 04:39:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    puts "#{array[0]}"
+    new_array = array.pop
+    array << "and #{new_array}"
+    array.join(", ")
+ end
 end
